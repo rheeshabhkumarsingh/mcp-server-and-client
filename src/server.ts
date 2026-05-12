4ab0cbac6b9f80d0e7ca8c57693f1cb67c8a27e2
rheeshabh <rheeshabhkumarsingh@gmail.com>
2026-05-12 23:17:40 +0530

    created first tool

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,33 @@
 // import { McpServer, StdioServerTransport } from '@modelcontextprotocol/server';
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { z } from "zod"
 
 const server = new McpServer({
     name: 'test',
     version: '1.0.0'
 })
 
+server.tool('create-user',
+    "create a new user in database",
+    {
+        name: z.string(),
+        email: z.string()
+    },
+    async (param)=>{
+        try {
+            return {
+                content: [
+                    {type:"text", text: "created user successfully"}
+                ]
+            }
+            
+        } catch (error) {
+            return {content: [
+                {type:"text", text: "failed to create user"}
+            ]
+        }}
+    })
 async function main() {
     try {
         const transport = new StdioServerTransport()
